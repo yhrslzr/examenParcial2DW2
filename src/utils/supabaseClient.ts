b70c27e1bb2c32e501ab88de6153e98671db7a51
Yahir Salazar <yahir.salazarortiz0113@gmail.com>
2026-05-08 17:35:16 -0600

    configuración de supabase

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const VITE_SUPABASE_URL="https://qkzfxvdbpcwntmbwhtcf.supabase.co"
+const VITE_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFremZ4dmRicGN3bnRtYndodGNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxODQ4NDEsImV4cCI6MjA5Mzc2MDg0MX0.i33Odjx_mznj2nOxOtepp14TEOQKQzU3I5r88GpFPpA"
+
+const supabaseUrl = VITE_SUPABASE_URL
+const supabaseKey = VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
